33eaee4861df490991d604b54723684c6b00058e
Matias Nabarro <matiasnabarro@gmail.com>
2022-11-07 15:37:59 +0100

    refactor: replace mock of Equipment for an instance of the class

--- java/src/test/java/codingdojo/PlayerTest.java
+++ java/src/test/java/codingdojo/PlayerTest.java
@@ -17,14 +17,10 @@ public class PlayerTest {
         Stats stats = mock(Stats.class);
         SimpleEnemy target = mock(SimpleEnemy.class);
         SimpleArmor armor = mock(SimpleArmor.class);
-        Equipment equipment = mock(Equipment.class);
         Item item = new BasicItem("",0,0);
+        Item swordOnRightHand = new BasicItem("flashy sword of danger",10,1f);
+        Equipment equipment = new Equipment(item, swordOnRightHand, item, item, item);
 
-        when(equipment.getRightHand()).thenReturn(new BasicItem("flashy sword of danger", 10,1f));
-        when(equipment.getLeftHand()).thenReturn(item);
-        when(equipment.getHead()).thenReturn(item);
-        when(equipment.getFeet()).thenReturn(item);
-        when(equipment.getChest()).thenReturn(item);
         when(inventory.getEquipment()).thenReturn(equipment);
         when(stats.getStrength()).thenReturn(0);
         when(target.getArmor()).thenReturn(armor);
