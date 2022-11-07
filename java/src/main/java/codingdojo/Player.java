c2fa30f1e58f4f2c5c1aca110006e7a2cd3ec12e
Matias Nabarro <matiasnabarro@gmail.com>
2022-11-07 17:24:08 +0100

    refactor: reformat Player class code

--- java/src/main/java/codingdojo/Player.java
+++ java/src/main/java/codingdojo/Player.java
@@ -2,66 +2,59 @@ package codingdojo;
 
 
 class Player extends Target {
-    private final Inventory inventory;
-    private final Stats stats;
 
-    Player(Inventory inventory, Stats stats) {
-        this.inventory = inventory;
-        this.stats = stats;
+  private final Inventory inventory;
+  private final Stats stats;
+
+  Player(Inventory inventory, Stats stats) {
+    this.inventory = inventory;
+    this.stats = stats;
+  }
+
+  private static int getSoakSimpleEnemy(SimpleEnemy target) {
+    int soak;
+    soak = Math.round(
+      target.getArmor().getDamageSoak() * (((float) target.getBuffs().stream().mapToDouble(Buff::soakModifier).sum()) + 1f));
+    return soak;
+  }
+
+  Damage calculateDamage(Target target) {
+    int totalDamage = getTotalDamage();
+    int soak = getSoak(target, totalDamage);
+    return new Damage(Math.max(0, totalDamage - soak));
+  }
+
+  private int getSoak(Target target, int totalDamage) {
+    int soak = 0;
+    if (target instanceof Player) {
+      soak = getSoakPlayerInstance(totalDamage);
+    } else if (target instanceof SimpleEnemy) {
+      soak = getSoakSimpleEnemy((SimpleEnemy) target);
     }
+    return soak;
+  }
 
-    Damage calculateDamage(Target target) {
-        int totalDamage = getTotalDamage();
-        int soak = getSoak(target, totalDamage);
-        return new Damage(Math.max(0, totalDamage - soak));
-    }
+  private int getSoakPlayerInstance(int totalDamage) {
+    // TODO: Not implemented yet
+    //  Add friendly fire
+    return totalDamage;
+  }
 
-    private int getSoak(Target target, int totalDamage) {
-        int soak = 0;
-        if (target instanceof Player) {
-            soak = getSoakPlayerInstance(totalDamage);
-        } else if (target instanceof SimpleEnemy) {
-            soak = getSoakSimpleEnemy((SimpleEnemy) target);
-        }
-        return soak;
-    }
+  private int getTotalDamage() {
+    int baseDamage = getBaseDamage();
+    float damageModifier = getDamageModifier();
+    return Math.round(baseDamage * damageModifier);
+  }
 
-    private int getSoakPlayerInstance(int totalDamage) {
-        // TODO: Not implemented yet
-        //  Add friendly fire
-        return totalDamage;
-    }
+  private float getDamageModifier() {
+    Equipment equipment = this.inventory.getEquipment();
+    float strengthModifier = stats.getStrength() * 0.1f;
+    return strengthModifier + equipment.getDamageModifier();
 
-    private static int getSoakSimpleEnemy(SimpleEnemy target) {
-        int soak;
-        soak = Math.round(
-            target.getArmor().getDamageSoak() *
-            (
-                ((float) target.getBuffs()
-                    .stream()
-                    .mapToDouble(Buff::soakModifier)
-                    .sum()) +
-                1f
-            )
-        );
-        return soak;
-    }
+  }
 
-    private int getTotalDamage() {
-        int baseDamage = getBaseDamage();
-        float damageModifier = getDamageModifier();
-        return Math.round(baseDamage * damageModifier);
-    }
-
-    private float getDamageModifier() {
-        Equipment equipment = this.inventory.getEquipment();
-        float strengthModifier = stats.getStrength() * 0.1f;
-        return strengthModifier + equipment.getDamageModifier();
-
-    }
-
-    private int getBaseDamage() {
-        Equipment equipment = this.inventory.getEquipment();
-        return equipment.getBaseDamage();
-    }
+  private int getBaseDamage() {
+    Equipment equipment = this.inventory.getEquipment();
+    return equipment.getBaseDamage();
+  }
 }
