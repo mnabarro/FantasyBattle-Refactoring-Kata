ba4011ae94b2cfc9a01451564e7aa2bfdef7afaf
Matias Nabarro <matiasnabarro@gmail.com>
2022-11-07 15:33:40 +0100

    test: test for "flashy sword of danger" passes OK

--- java/src/test/java/codingdojo/PlayerTest.java
+++ java/src/test/java/codingdojo/PlayerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class PlayerTest {
 
@@ -15,9 +16,23 @@ public class PlayerTest {
         Inventory inventory = mock(Inventory.class);
         Stats stats = mock(Stats.class);
         SimpleEnemy target = mock(SimpleEnemy.class);
+        SimpleArmor armor = mock(SimpleArmor.class);
+        Equipment equipment = mock(Equipment.class);
+        Item item = new BasicItem("",0,0);
+
+        when(equipment.getRightHand()).thenReturn(new BasicItem("flashy sword of danger", 10,1f));
+        when(equipment.getLeftHand()).thenReturn(item);
+        when(equipment.getHead()).thenReturn(item);
+        when(equipment.getFeet()).thenReturn(item);
+        when(equipment.getChest()).thenReturn(item);
+        when(inventory.getEquipment()).thenReturn(equipment);
+        when(stats.getStrength()).thenReturn(0);
+        when(target.getArmor()).thenReturn(armor);
+        when(armor.getDamageSoak()).thenReturn(5);
 
         Damage damage = new Player(inventory, stats).calculateDamage(target);
-        assertEquals(10, damage.getAmount());
+
+        assertEquals(5, damage.getAmount());
     }
 
     // choose this one if you are not familiar with mocks
