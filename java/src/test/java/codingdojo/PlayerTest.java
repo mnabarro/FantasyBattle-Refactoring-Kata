6bc428017ecf7d9331c001d895d5481ce1d28c6c
Matias Nabarro <matiasnabarro@gmail.com>
2022-11-08 09:33:24 +0100

    refactor: modify List initialization to get rid of a warning. Test code reformat

--- java/src/test/java/codingdojo/PlayerTest.java
+++ java/src/test/java/codingdojo/PlayerTest.java
@@ -1,6 +1,5 @@
 package codingdojo;
 
-import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,28 +8,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlayerTest {
 
-    Stats stats;
-    SimpleArmor armor;
-    Item notUsedItem;
-    BasicBuff buff;
-    List<Buff> buffs;
-    SimpleEnemy target;
-    @BeforeEach
-    void setup () {
-        stats = new Stats(10);
-        armor = new SimpleArmor(5);
-        notUsedItem = new BasicItem("",0,0);
-        buff = new BasicBuff(1,1);
-        buffs = Arrays.asList(buff);
-        target = new SimpleEnemy(armor,buffs);
-    }
-    @Test
-    void calculateDamageProducedByWithFlashySwordOfDanger() {
-        Item swordOnRightHand = new BasicItem("flashy sword of danger",10,1f);
-        Equipment equipment = new Equipment(notUsedItem, swordOnRightHand, notUsedItem, notUsedItem, notUsedItem);
+  Stats stats;
+  SimpleArmor armor;
+  Item notUsedItem;
+  BasicBuff buff;
+  List<Buff> buffs;
+  SimpleEnemy target;
 
-        Damage damage = new Player(equipment, stats).calculateDamage(target);
+  @BeforeEach
+  void setup() {
+    stats = new Stats(10);
+    armor = new SimpleArmor(5);
+    notUsedItem = new BasicItem("", 0, 0);
+    buff = new BasicBuff(1, 1);
+    buffs = List.of(buff);
+    target = new SimpleEnemy(armor, buffs);
+  }
 
-        assertEquals(10, damage.getAmount());
-    }
+  @Test
+  void calculateDamageProducedByWithFlashySwordOfDanger() {
+    Item swordOnRightHand = new BasicItem("flashy sword of danger", 10, 1f);
+    Equipment equipment = new Equipment(notUsedItem, swordOnRightHand, notUsedItem, notUsedItem, notUsedItem);
+
+    Damage damage = new Player(equipment, stats).calculateDamage(target);
+
+    assertEquals(10, damage.getAmount());
+  }
 }
