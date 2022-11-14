4cc1b0f548a530323ca3c09769655de53218f429
Matias Nabarro <matiasnabarro@gmail.com>
2022-11-14 09:38:59 +0100

    refactor: WIP move getSoakModifier method to Buffs class

--- java/src/main/java/codingdojo/SimpleEnemy.java
+++ java/src/main/java/codingdojo/SimpleEnemy.java
@@ -4,21 +4,17 @@ import java.util.List;
 public class SimpleEnemy extends Target {
 
     private final Armor armor;
-    private final List<Buff> buffs;
+    private final Buffs buffs;
 
-    public SimpleEnemy(Armor armor, List<Buff> buffs) {
+    public SimpleEnemy(Armor armor, Buffs buffs)  {
         this.armor = armor;
         this.buffs = buffs;
     }
 
     int getSoak() {
-        return Math.round(getArmorDamageSoak() * ((getSoakModifier()) + 1f));
+        return Math.round(getArmorDamageSoak() * ((buffs.getSoakModifier()) + 1f));
     }
     private int getArmorDamageSoak() {
         return armor.getDamageSoak();
-    }
-
-    private float getSoakModifier() {
-        return (float) buffs.stream().mapToDouble(Buff::soakModifier).sum();
     }
 }
